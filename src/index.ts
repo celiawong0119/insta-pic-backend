83ee936447e7c72bee6845daa3c9739edd2c2a57
celiawong0119 <68861940+celiawong0119@users.noreply.github.com>
2021-11-02 15:08:34 +0800

    remove debug middleware

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.log(req);
-  next();
-});
-
 // set up cors for dev to "Origin is not allowed by Access-Control-Allow-Origin" error
 // use white list for production
 app.use(
